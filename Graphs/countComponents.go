685e9c90b09eacb9358444679c204b33a67eb354
purvaz <pzinjarde@sfsu.edu>
2025-01-30 12:50:27 -0800

    Explanation added

--- Graphs/countComponents.go
+++ Graphs/countComponents.go
@@ -5,12 +5,16 @@ type DSU struct {
 	rank   []int
 }
 
+// Create a struct that will have the parent and rank array.
 func NewDSU(n int) *DSU {
 
 	dsu := &DSU{
 		parent: make([]int, n),
 		rank:   make([]int, n),
 	}
+	// Populate the parent and rank arrays.
+	// The parent array will hold the parent to the current node,
+	// whereas the rank contains the number of children that the current node has.
 	for i := 0; i < n; i++ {
 		dsu.parent[i] = i
 		dsu.rank[i] = 1
@@ -21,7 +25,9 @@ func NewDSU(n int) *DSU {
 func (dsu *DSU) Find(node int) int {
 
 	current := node
+	// Check if the parent of current node is the node itself. If not, iterate till we find such a node.
 	for current != dsu.parent[current] {
+		// Assign the root parent as the parent of the node instead of the immediate parent.
 		dsu.parent[current] = dsu.parent[dsu.parent[current]]
 		current = dsu.parent[current]
 	}
@@ -33,9 +39,11 @@ func (dsu *DSU) Union(node1, node2 int) bool {
 	parent1 := dsu.Find(node1)
 	parent2 := dsu.Find(node2)
 
+	// If the parents are same
 	if parent1 == parent2 {
 		return false
 	}
+	// Check the parent hierarchy and assign the rank and the parent to the nodes.
 	if dsu.rank[parent2] > dsu.rank[parent1] {
 		parent1, parent2 = parent2, parent1
 	}
@@ -51,8 +59,11 @@ func countComponents(n int, edges [][]int) int {
 
 	result := n
 
+	// For every edge, call the union function
 	for _, edge := range edges {
 		u, v := edge[0], edge[1]
+		// If the union function returns a true, decrement the result.
+		// This means that the nodes are connected.
 		if dsu.Union(u, v) {
 			result--
 		}
