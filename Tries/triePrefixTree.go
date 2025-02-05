dcc5e2afa2bc485bbaad49998eb1145a12672968
purvaz <pzinjarde@sfsu.edu>
2025-02-05 12:24:31 -0800

    Tries Code

--- Tries/triePrefixTree.go
+++ Tries/triePrefixTree.go
@@ -1,22 +1,53 @@
 package Tries
 
+type TrieNode struct {
+	children  map[rune]*TrieNode
+	endOfWord bool
+}
+
 type Trie struct {
+	root *TrieNode
 }
 
 func Constructor() Trie {
-
+	return Trie{root: &TrieNode{children: make(map[rune]*TrieNode)}}
 }
 
 func (this *Trie) Insert(word string) {
 
+	current := this.root
+
+	for _, char := range word {
+		if current.children[char] == nil {
+			current.children[char] = &TrieNode{children: make(map[rune]*TrieNode)}
+		}
+		current = current.children[char]
+	}
+	current.endOfWord = true
 }
 
 func (this *Trie) Search(word string) bool {
 
+	current := this.root
+	for _, char := range word {
+		if current.children[char] == nil {
+			return false
+		}
+		current = current.children[char]
+	}
+	return current.endOfWord
 }
 
 func (this *Trie) StartsWith(prefix string) bool {
 
+	current := this.root
+	for _, char := range prefix {
+		if current.children[char] == nil {
+			return false
+		}
+		current = current.children[char]
+	}
+	return true
 }
 
 /**
