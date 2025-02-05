bb90ceccc71ed276c997ae3dd25c5ece2c602cf0
purvaz <pzinjarde@sfsu.edu>
2025-02-04 16:32:17 -0800

    Explanation added

--- Graphs/wordLadder.go
+++ Graphs/wordLadder.go
@@ -2,13 +2,17 @@ package Graphs
 
 func ladderLength(beginWord string, endWord string, wordList []string) int {
 
+	// check if the end word is in the word list
 	if !contains(wordList, endWord) {
 		return 0
 	}
 
+	// make an adjacency list that will store all the patterns and the words that match the pattern
 	neighbors := make(map[string][]string)
+	// append the begin word to the wordlist
 	wordList = append(wordList, beginWord)
 
+	// traverse through the word list, and create the patterns for that word
 	for _, word := range wordList {
 		for i := 0; i < len(word); i++ {
 			pattern := word[:i] + "*" + word[i+1:]
@@ -16,26 +20,32 @@ func ladderLength(beginWord string, endWord string, wordList []string) int {
 		}
 	}
 
+	// create a visited map that will check if the particular word has been visited
 	visited := make(map[string]bool)
 	visited[beginWord] = true
 
 	result := 1
+	// add the first word in the queue
 	queue := []string{beginWord}
 
+	// perform BFS on the queue
 	for len(queue) > 0 {
 		for i := len(queue); i > 0; i-- {
 			word := queue[0]
 			queue = queue[1:]
 
+			// check if the word matches the end word
 			if word == endWord {
 				return result
 			}
 
 			for j := 0; j < len(word); j++ {
+				// create the patterns for the particular word,
 				pattern := word[:j] + "*" + word[j+1:]
 				for _, neighborWord := range neighbors[pattern] {
 					if !visited[neighborWord] {
 						visited[neighborWord] = true
+						// add the words that match the pattern in the queue
 						queue = append(queue, neighborWord)
 					}
 				}
