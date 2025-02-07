5ea40d3329268ad04a85aa4b6b10bdd220f9e2ba
purvaz <pzinjarde@sfsu.edu>
2025-02-07 12:36:02 -0800

    Word Dict

--- Tries/wordDictionary.go
+++ Tries/wordDictionary.go
@@ -1,5 +1,12 @@
 package Tries
 
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.AddWord(word);
+ * param_2 := obj.Search(word);
+ */
+
 type WordDictionary struct {
 }
 
@@ -14,10 +21,3 @@ func (this *WordDictionary) AddWord(word string) {
 func (this *WordDictionary) Search(word string) bool {
 
 }
-
-/**
- * Your WordDictionary object will be instantiated and called as such:
- * obj := Constructor();
- * obj.AddWord(word);
- * param_2 := obj.Search(word);
- */
