180ae2f313d04f6d5a8a9227f025ce5279396210
purvaz <pzinjarde@sfsu.edu>
2025-02-17 18:11:31 -0800

    Solution using recursion

--- DynamicProgramming2D/interleavingStrings.go
+++ DynamicProgramming2D/interleavingStrings.go
@@ -2,5 +2,27 @@ package DynamicProgramming2D
 
 func isInterleave(s1 string, s2 string, s3 string) bool {
 
-	return true
+	var dfs func(i, j, k int) bool
+	dfs = func(i, j, k int) bool {
+		if k == len(s3) {
+			return i == len(s1) && j == len(s2)
+		}
+
+		if i < len(s1) && s1[i] == s3[k] {
+			if dfs(i+1, j, k+1) {
+				return true
+			}
+		}
+
+		if j < len(s2) && s2[j] == s3[k] {
+			if dfs(i, j+1, k+1) {
+				return true
+			}
+		}
+
+		return false
+	}
+
+	return dfs(0, 0, 0)
+
 }
